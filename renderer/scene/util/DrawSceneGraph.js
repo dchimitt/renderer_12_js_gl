7348b1ed8bcf63ad64f63ade5be77cf2b1001f4f
ejvogt5 <vogte@pnw.edu>
2024-10-30 14:14:25 -0500

    modelToDot - file done but untested
    need to add a quick test under the test HTML to see if it actually draws anything

--- renderer/scene/util/DrawSceneGraph.js
+++ renderer/scene/util/DrawSceneGraph.js
@@ -191,7 +191,160 @@ export default class DrawSceneGraph {
         return result
     }
 
-    // TODO:  modelToDot
+    static modelToDot( model, nodeName ) {
+        let result = ""
 
-    // TODO:  DrawSceneGraph (ctor)
+        if ( this.drawMatrix || this.drawMatrixDetails ) {
+            // Matrix node name
+            let tNodeName = nodeName + "_Matrix"
+
+            // Matrix node and label
+            result += tNodeName + " "
+
+            if ( this.drawMatrixDetails ) {
+                result += `[label="Matrix:\n${ mode.getMatrix() }"];\n`
+            } else {
+                result += `[label="Matrix"];\n`
+            }
+
+            // Matrix edge
+            result += `${ nodeName } -> ${ tNodeName };\n`
+        }
+
+        if ( this.drawVertexList ) {
+            // Array<Vertex> node and label
+            let vertexListNodeName = nodeName + "_vertexList"
+            result += vertexListNodeName + " "
+            result += `[label="Array<Vertex>"];\n`
+
+            // Array<Vertex> edge
+            result += `${ nodeName } -> ${ vertexListNodeName };\n`
+
+            // Array<Vertex> children
+            let vertexCounter = 0
+            let lastVertexNodeName = vertexListNodeName
+            for ( let v of model.vertexList ) {
+                // vertex node name
+                let vertexNodeName = nodeName + "_v" + vertexCounter
+
+                // vertex node and label
+                result += vertexNodeName + " "
+                result += `[label="Vertex: ${ v }"];\n]`
+
+                // vertex edge
+                result += `${ lastVertexNodeName } -> ${ vertexNodeName };\n`
+
+                lastVertexNodeName = vertexNodeName
+                ++vertexCounter
+            }
+
+            // Array<Color> node and label
+            let colorListNodeName = nodeName + "_colorList"
+            result += colorListNodeName = " "
+            result += `[label="Array<Color>"];\n`
+
+            // Array<Color> edge
+            result += `${ nodeName } -> ${ colorListNodeName };\n`
+
+            // Array<Color> children
+            let colorCounter = 0
+            let lastColorNodeName = colorListNodeName
+            for ( let c of model.colorList ) {
+                // color node name
+                let colorNodeName = nodeName + "_c" + colorCounter
+
+                // color node and label
+                result += colorNodeName + " "
+                result += `[label="${ c }"];\n`
+
+                // color edge
+                result += `${ lastColorNodeName } -> ${ colorNodeName };\n`
+
+                lastColorNodeName = colorNodeName
+                ++colorCounter
+            }
+
+            // Array<Primitive> node and label
+            let primitiveListNodeName = nodeName + "_primitiveList"
+            result += primitiveListNodeName + " "
+            result += `[label="Array<Primitive>"];\n`
+
+            // Array<Primitive> edge
+            result += `${ nodeName } -> ${ primitiveListNodeName };\n`
+
+            // Array<Primitive> children
+            let primitiveCounter = 0
+            let lastPrimitiveNodeName = primitiveListNodeName
+            for ( let p of model.primitiveList ) {
+                // primitive node name
+                let primitiveNodeName = nodeName + "_p" + primitiveCounter
+
+                // primitive node and label
+                result += primitiveNodeName + " "
+                result += `[label="${ p }"];\n`
+
+                // primitive edge
+                result += `${ lastPrimitiveNodeName } -> ${ primitiveNodeName };\n`
+
+                lastPrimitiveNodeName = primitiveNodeName
+                primitiveCounter++
+            }
+        }
+
+        // recursively conver this model's nested models
+        // into a dot description
+        if ( model.nestedModels.length !== 0 ) {
+            // nested model list node and label
+            let nestedModelListNodeName = nodeName + "_List "
+            result += nestedModelListNodeName + " "
+            result += `[label="Array<Model>"];\n`
+
+            // nested model list edge
+            result += `${ nodeName } -> ${ nestedModelListNodeName };\n`
+
+            for ( let i = 0; i < model.nestedModels.length; ++i ) {
+                // nested model
+                let modelReference = model.getNestedModel( i )
+
+                // check if the model is being reused
+                let modelVisited = this.visitedNodes.find( modelReference ) !== undefined
+
+                if ( !modelVisited ) {
+                    // nested model node name
+                    ++this.nodeNumber
+                    let mNodeName = "_m" + this.nodeNumber
+
+                    // mark this model as visited
+                    this.visitedNodes.push( modelReference )
+                    this.visitedNodeNames.push( mNodeName )
+
+                    // nested model node and label
+                    result += mNodeName + " "
+                    result += `[label="Model: ${ modelReference.name }"];\n`
+
+                    // nested model edge
+                    result += `${ nestedModelListNodeName } -> ${ mNodeName };\n`
+
+                    // the nested model's matrix and nested models
+                    result += this.modelToDot( modelReference, mNodeName )
+                } else {
+                    let index = this.visitedNodes.indexOf( modelReference )
+
+                    // nested model node name
+                    let mNodeName = this.visitedNodeNames[ index ]
+
+                    // nested model edge
+                    result += `${ nestedModelListNodeName } -> ${ mNodeName };\n`
+                }
+            }
+        }
+
+        return result
+    }
+
+    // intention:  enforce noninstantiable class in JS
+    // TODO:  check if this is even right
+    DrawSceneGraph() {
+        throw new Error()
+    }
 }
